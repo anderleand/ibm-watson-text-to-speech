f1dc9473533bb0a459b55fe8a3c8467da5c9fa43
anderleand <andersonleandro.desouza@gmail.com>
2021-04-28 19:44:51 -0300

    Update Tabelas.js
    Refatoração
    Substituído 'CREATE TABLE' por 'CREATE TABLE IF NOT EXISTS'.

--- text-to-speech/infraestrutura/Tabelas.js
+++ text-to-speech/infraestrutura/Tabelas.js
@@ -1,16 +1,16 @@
 class Tabelas {
-    init(conexao){
+    init(conexao) {
         this.conexao = conexao;
         this.criaTabela();
     }
 
-    criaTabela(){
-        const sql = 'CREATE TABLE comentarios (id int NOT NULL AUTO_INCREMENT, comentario varchar(140) NOT NULL, PRIMARY KEY(id))';
+    criaTabela() {
+        const sql = 'CREATE TABLE IF NOT EXISTS comentarios (id int NOT NULL AUTO_INCREMENT, comentario varchar(140) NOT NULL, PRIMARY KEY(id))';
 
         this.conexao.query(sql, erro => {
-            if(erro){
+            if (erro) {
                 console.log(erro);
-            }else{
+            } else {
                 console.log('Tabela comentarios criada com sucesso!');
             }
         });
