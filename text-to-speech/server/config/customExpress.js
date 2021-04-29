f5c96702b4b4a0e93de470516285ff1596a3bcd1
anderleand <andersonleandro.desouza@gmail.com>
2021-04-29 10:49:03 -0300

    Update customExpress.js
    Permitir acesso a API com CORS

--- text-to-speech/server/config/customExpress.js
+++ text-to-speech/server/config/customExpress.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const consign = require('consign');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 module.exports = () => {
 
@@ -8,6 +9,12 @@ module.exports = () => {
 
     app.use(bodyParser.urlencoded({ extended: true }));
     app.use(bodyParser.json());
+
+    app.use((req, res, next) => {
+        res.header('Access-Control-Allow-Origin', '*');
+        app.use(cors());
+        next();
+    });
 
     consign()
         .include('controllers')
