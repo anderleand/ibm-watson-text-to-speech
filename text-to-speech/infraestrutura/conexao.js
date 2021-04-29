245c751515c1cc8e64ff5bcac024d21d01101d38
anderleand <andersonleandro.desouza@gmail.com>
2021-04-28 21:33:32 -0300

    Update conexao.js

--- text-to-speech/infraestrutura/conexao.js
+++ text-to-speech/infraestrutura/conexao.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql');
 
 const conexao = mysql.createConnection({
-    host:'localhost',
+    host: 'localhost',
     port: '3306',
     user: 'root',
-    password:'admin',
-    database:'text-to-speech'
+    password: 'admin',
+    database: 'text-to-speech'
 });
 
 module.exports = conexao;
