e8424ef1d67715bc4042f74c37b456cb077b2458
anderleand <andersonleandro.desouza@gmail.com>
2021-04-30 01:49:07 -0300

    Create postarComentario.js

---
+++ text-to-speech/client/js/postarComentario.js
@@ -0,0 +1,34 @@
+const botaoCadastrar = document.querySelector('#botao-cadastrar');
+const textArea = document.querySelector('#textarea-comentario');
+
+botaoCadastrar.addEventListener('click', () => {
+    //event.preventDefault();
+    onFormBubmit();
+
+    function onFormBubmit() {
+        const comentarios =readFormData();
+        console.log(comentarios)
+        console.log(JSON.stringify(comentarios))
+    }
+
+    function readFormData() {
+        var formData = {};
+        formData['"comentario"']=textArea.value;
+        return formData;
+    }
+
+    const param = `comentario=${textArea.value}`;
+    const xhr = new XMLHttpRequest();
+    xhr.open('POST', 'http://localhost:3000', true);
+    xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+
+    xhr.onreadystatechange = function () {
+        if(xhr.readyState == 4 && xhr.status == 200){
+            alert(xhr.responseText);
+        }
+    }
+    xhr.send(param)
+
+    document.location.reload(true);
+
+});
