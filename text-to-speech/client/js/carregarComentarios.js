af82cb11138a009ccfec63d10c0a48929bbc7414
anderleand <andersonleandro.desouza@gmail.com>
2021-04-30 04:31:26 -0300

    Update carregarComentarios.js
    Finalização da parte de carregamento

--- text-to-speech/client/js/carregarComentarios.js
+++ text-to-speech/client/js/carregarComentarios.js
@@ -2,12 +2,12 @@ const template = (comentario, id) => {
 
     const divNovoComentario = document.createElement('div');
     divNovoComentario.classList.add("ouvir-comentario");
-    
+
     const conteudo = `
      <p id="comentarios" class="comentarios-texto">${comentario}</p>
-     <button class="botao botao-ouvir" onclick="document.getElementById('playid').play()">Ouvir</button>
+     <button class="botao botao-ouvir" onclick=document.getElementById("${id}").play()>Ouvir</button>
     <audio id="${id}">
-    <source src="audio/${id}.wav" type="audio/wav" />
+    <source src="./../server/public/audio/${id}.wav" type="audio/wav" />
     </audio>
     `
     divNovoComentario.innerHTML = conteudo;
@@ -15,7 +15,6 @@ const template = (comentario, id) => {
 }
 
 const comentariosView = document.querySelector('#comentariosView');
-console.log(comentariosView)
 
 window.onload = function carregarComentarios() {
 
@@ -24,25 +23,13 @@ window.onload = function carregarComentarios() {
     xhr.send();
     xhr.onload = () => {
 
-        const comentarios = JSON.parse(xhr.response);
-        console.log(comentarios)
-        comentarios.forEach(elemento => {
-            comentariosView.appendChild(template(elemento.comentario, elemento.id));
-        });
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            const comentarios = JSON.parse(xhr.response);
+            comentarios.forEach(elemento => {
+                comentariosView.appendChild(template(elemento.comentario, elemento.id));
+            });
+        }
 
     }
 
-    // const xhr = new XMLHttpRequest();
-    // xhr.open('GET', 'http://localhost:3000', true);
-
-    // xhr.onreadystatechange = function () {
-    //     if (this.readyState == 4 && this.status == 200) {
-    //         console.log('Conexão OK!')
-    //         const comentarios = JSON.parse(this.responseText)
-
-    //         console.log(comentarios);
-    //     }
-    // }
-
-    // xhr.send()
 };
