1c75b9c7ad4565155dcb23aeb452cdd796d51763
anderleand <andersonleandro.desouza@gmail.com>
2021-04-30 13:02:14 -0300

    Update Comentario.js
    Fazendo a chamado do método criaAudio logo após a inserção do comentário no banco de dados

--- text-to-speech/server/models/Comentario.js
+++ text-to-speech/server/models/Comentario.js
@@ -1,15 +1,21 @@
 const conexao = require('../infraestrutura/conexao');
+const TextToSpeech = require('../models/TextToSpeech.js');
 
 class Comentario {
 
     adiciona(comentario, res) {
         const sql = 'INSERT INTO Comentarios SET ?';
-
         conexao.query(sql, comentario, (erro, resultado) => {
+
+            let criaAudioId = resultado.insertId;
+            let textComentario = comentario;
+            let criaAudioComentario = textComentario.comentario;
+            
             if (erro) {
                 res.status(400).json(erro);
             } else {
-                res.status(200).json(comentario);
+                res.status(201).json(comentario);
+                TextToSpeech.criaAudio(criaAudioId, criaAudioComentario);
             }
         });
     }
@@ -28,7 +34,6 @@ class Comentario {
 
     buscaPorId(id, res) {
         const sql = `SELECT * FROM Comentarios WHERE id=${id}`;
-
         conexao.query(sql, (erro, resultado) => {
             const comentario = resultado[0];
 
